9b1a66d3767d866ddd07f2368b409787de4335a0
kowshicahmed <kowshicahmed@yahoo.com>
2021-07-12 20:51:29 +0200

    basic two projects push

---
+++ 01_basic_array_to_func/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/* * How to pass an array to a function
+ *  */
+
+/********** Function Prototypes ***************/
+void print_array(const int arr[], size_t size);    //< Prints the values of an array that was passed
+void set_array(int arr[], size_t size, int value); //< Sets the values of an array given the size and members
+
+/********** Main method ***************/
+int main(int argc, char **argv)
+{
+    int numbers[]{1, 2, 3, 4, 5};
+
+    print_array(numbers, 5);
+    set_array(numbers, 5, 100);
+    print_array(numbers, 5);
+
+    return 0;
+}
+
+/********** Helper Functions ***************/
+void print_array(const int arr[], size_t size)
+{
+    for (size_t i{0}; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void set_array(int arr[], size_t size, int value)
+{
+    for (size_t i{0}; i < size; ++i)
+    {
+        arr[i] = value;
+    }
+}
---
+++ 01_basic_char_funcs/main.cpp
@@ -0,0 +1,38 @@
+#include <cctype> //< This header allows testing and converting chars
+#include <iostream>
+
+/* * Some basic character functions of cpp
+ *  */
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a{};
+
+    a = isalpha('C'); //< True if C is a letter
+    cout << a << endl;
+
+    a = isalnum('C'); //< True if C is a letter of digit
+    cout << a << endl;
+
+    a = isdigit('C'); //< True if C is a digit
+    cout << a << endl;
+
+    a = islower('C'); //< True if C is a lowercase letter
+    cout << a << endl;
+
+    a = isprint('C'); //< True if C is a printable character
+    cout << a << endl;
+
+    a = ispunct('C'); //< True if C is a punctutation character
+    cout << a << endl;
+
+    a = isupper('C'); //< True if C is an uppercase letter
+    cout << a << endl;
+
+    a = isspace('C'); //< True if C is whitespace
+    cout << a << endl;
+
+    return 0;
+}
